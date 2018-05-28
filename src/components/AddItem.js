b75f8bf7ac8d68c093211f228f2b68330b8e2598
Anne Jizrelle Jose <annejizrelle@gmail.com>
2018-05-29 02:53:11 +0800

    AddItem Refactor

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -4,15 +4,18 @@ class AddItem extends React.Component {
 
   // STATE
   state = {
+    item: {},
     items: {},
     errors: {
-      idErrorMsg: '',
-      nameErrorMsg: '',
-      priceErrorMsg: '',
-      quantityErrorMsg: '',
+      hasError: false,
+      messages: {
+        id: '',
+        name: '',
+        price: '',
+        quantity: '',
+      },
     }
   };
-  
 
   // ITEM CREATE REFS
   idRef = React.createRef();
@@ -20,55 +23,22 @@ class AddItem extends React.Component {
   priceRef = React.createRef();
   quantityRef = React.createRef();
 
+  constructor() {
+    super();
 
-  // VALIDATE FORM FUNCTION
-  validateForm = (item) => {
-    // making a copy of the errors state
-    const errors = {...this.state.errors};
-
-    // assigning error message
-    const foundItem = Object.keys(this.state.items).find((key) => {
-      return (this.state.items[key].id === item.id);
-    });        
-
-    if(foundItem) {
-      errors.idErrorMsg = `The ID ${item.id} is already existing.`;
-    } else {
-      errors.idErrorMsg = ``;
-    }
-
-    if(item.name === '') {
-      errors.nameErrorMsg = `The name must not be blank.`;
-    } else {
-      errors.nameErrorMsg = ``;
-    }
-
-    if(item.price === '' || isNaN(item.price)) {
-      errors.priceErrorMsg = `The price must not be blank.`;
-    } else {
-      errors.priceErrorMsg = ``;
-    }
-
-    if(item.quantity === '' || isNaN(item.quantity)) {
-      errors.quantityErrorMsg = `The quantity must not be blank.`;
-    } else {
-      errors.quantityErrorMsg = ``;
-    }
-    console.log(errors, item);
-
-    // saving the error messages on the state from the copy
-    this.setState({
-      errors
-    });
-
-    // validating before saving to local storage
-    if(errors.idErrorMsg === '' && errors.nameErrorMsg === '' && errors.priceErrorMsg === '' && errors.quantityErrorMsg === '') {
-      this.saveToLocalStorage(item);
+    const localStorageRef = localStorage.getItem('inventory');
+    if(localStorageRef) {
+      this.state.items = JSON.parse(localStorageRef);
     }
   }
 
-
-  // ADD ITEM FUNCTION
+  /**
+   * Add a new item 
+   * Calls validateForm and assignItem functions
+   * 
+   * @param object event
+   * @returns void
+   */
   addItem = (event) => {
     event.preventDefault();
     
@@ -80,60 +50,119 @@ class AddItem extends React.Component {
       quantity: parseFloat(this.quantityRef.value.value)
     };
 
-    // calling validateForm function
-    this.validateForm(item); 
-  };
+    this.validateForm(item);
 
-
-  // SAVE TO LOCAL STORAGE FUNCTION
-  saveToLocalStorage = (item) => {
-    // 1. making a copy of the items state
-    const items = {...this.state.items};
-    // 2. assigning unique key
-    items[Date.now()] = item;
-    // 3. saving items to local storage
-    localStorage.setItem('inventory', JSON.stringify(items));
-
-    // going back to inventory page
-    this.props.history.push('/inventory');
+    this.assignItem(item);
   }
 
-  componentDidMount() {
-    const localStorageRef = localStorage.getItem('inventory');
-    if(localStorageRef) {
-      this.setState({
-        items: JSON.parse(localStorageRef),
-      });
+  /**
+   * Validates form
+   * Updates errors state
+   * 
+   * @param object item
+   * @returns void
+   */
+  validateForm = (item) => {
+    const errors = {...this.state.errors};
+    const messages = errors.messages;
+    errors.hasError = false;
+
+    const foundItem = Object.keys(this.state.items).find((key) => {
+      return (this.state.items[key].id === item.id);
+    });        
+
+    if(foundItem) {
+      messages.id = `The ID ${item.id} is already existing.`;
+      errors.hasError = true;
+    } else {
+      messages.id = '';
     }
+
+    if(item.name === '') {
+      messages.name = `The name must not be blank.`;
+      errors.hasError = true;
+    } else {
+      messages.name = '';
+    }
+
+    if(item.price === '' || isNaN(item.price)) {
+      messages.price = `The price must not be blank.`;
+      errors.hasError = true;
+    } else {
+      messages.price = '';
+    }
+
+    if(item.quantity === '' || isNaN(item.quantity)) {
+      messages.quantity = `The quantity must not be blank.`;
+      errors.hasError = true;
+    } else {
+      messages.quantity = '';
+    }
+
+    this.setState({ errors });
+  }
+
+  /**
+   * This assigns item to state
+   * 
+   * @param object newItem
+   * @returns void
+   */
+  assignItem = (newItem) => {
+    this.setState({item: newItem});
+  }
+
+  /**
+   * Makes a copy of the items state
+   * Assigns unique key for each item
+   * Saves added item to the local storage
+   * 
+   * @param object item
+   * @returns void
+   */
+  saveToLocalStorage = (item) => {
+    const items = {...this.state.items};
+    items[Date.now()] = item;
+    localStorage.setItem('inventory', JSON.stringify(items));
+  }
+
+  /**
+   * @inheritdoc
+   */
+  componentDidUpdate() {
+    const errors = this.state.errors;
+
+    if(!errors.hasError) {
+      this.saveToLocalStorage(this.state.item);
+      this.props.history.push('/inventory');
+    } 
   }
 
   render() {
     return (
 
       <div>
-
         <h2>Add Item</h2>
 
         <form onSubmit={this.addItem}>
           <label htmlFor="id">Item ID Number:</label>
           <input type="text" name="id" ref={this.idRef}/>
-          <p>{this.state.errors.idErrorMsg}</p>
+          <p>{this.state.errors.messages.id}</p>
 
           <label htmlFor="name">Item Name:</label>
           <input type="text" name="name" ref={this.nameRef}/>
-          <p>{this.state.errors.nameErrorMsg}</p>
+          <p>{this.state.errors.messages.name}</p>
 
           <label htmlFor="price">Item Price:</label>
           <input type="text" name="price" ref={this.priceRef}/>
-          <p>{this.state.errors.priceErrorMsg}</p>
+          <p>{this.state.errors.messages.price}</p>
 
           <label htmlFor="quantity">Initial Quantity</label>
           <input type="number" name="quantity" ref={this.quantityRef}/>
-          <p>{this.state.errors.quantityErrorMsg}</p>
+          <p>{this.state.errors.messages.quantity}</p>
 
           <button type="submit">Add Item</button>
         </form>
-
       </div>
    
     )
